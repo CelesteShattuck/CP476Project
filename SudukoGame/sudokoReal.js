779ed67c6e2047d5b6624b43c88f0949228529c9
Celeste <jacob.sh99@gmail.com>
2021-04-05 15:55:48 -0400

    Update sudokoReal.js

--- SudukoGame/sudokoReal.js
+++ SudukoGame/sudokoReal.js
@@ -1,20 +1,16 @@
-drawBoard = function(){
-    var index = 0,
-        position       = { x: 0, y: 0 },
-        group_position = { x: 0, y: 0 };
-    
+drawBoard = function(){    
     var sudoku_board = $('<div></div>').addClass('sudoku_board');    
     $('#'+ this.id).empty();
     
     var puzzle = 1 //get from file
     var answer = 1 //get from json
     //draw board 
-    for (i=0; i < this.nn; i++) {
-        for (j=0; j < this.nn; j++) {
+    for (i=0; i < 9; i++) {
+        for (j=0; j < 9; j++) {
             position       = { x: i+1, y: j+1 };
             group_position = { x: Math.floor((position.x -1)/this.n), y: Math.floor((position.y-1)/this.n) };
             
-            var value = (puzzle[index] > 0 ? this.board[index] : ''),
+            var value = (puzzle[index] > 0 ? puzzle[index] : ''),
                 cell = $('<div></div>')
                             .addClass('cell')
                             .attr('x', position.x)
